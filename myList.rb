e064b6e58275778e9136b992898c0f337b141884
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-14 19:57:00 +0300

    Create: class

---
+++ myList.rb
@@ -0,0 +1,13 @@
+class MyList
+  @list=[]
+  def initialize(*input)
+    @list=input
+  end
+  def list
+    puts @list
+  end
+end
+
+puts 'this is working'
+my_list=MyList.new(1,2,3,4,);
+puts my_list.list
