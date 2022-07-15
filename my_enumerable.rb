fe124a55fae54239c7b33eea131cb7e59df402a1
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-15 11:42:20 +0300

    Complete all test cases

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,11 +1,11 @@
 module Enumerable
   def all?
-    return false unless block_given?
+    return true unless block_given?
     each {|n| return false unless yield(n)}
     true
   end
   def any?
-    return false unless block_given?
+    return true unless block_given?
     each {|n| return true if yield(n)}
     false
   end
--- my_list.rb
+++ my_list.rb
@@ -13,18 +13,18 @@ end
 list = MyList.new(1, 2, 3, 4)
 
 # Test #all?
-# result = list.all? { |e| e < 5 } # true
-# puts result
-# result = list.all? { |e| e > 5 } # false
-# puts result
-# result = list.all? # true
-# puts result
+result = list.all? { |e| e < 5 } # true
+puts result
+result = list.all? { |e| e > 5 } # false
+puts result
+result = list.all? # true
+puts result
 
 # Test #any?
-# result = list.any? { |e| e == 2 } # true
-# puts result
-# result = list.any? { |e| e == 5 } # false
-# puts result
+result = list.any? { |e| e == 2 } # true
+puts result
+result = list.any? { |e| e == 5 } # false
+puts result
 
 # Test #filter
 result = list.filter { |e| e&.even? } # 2, 4
